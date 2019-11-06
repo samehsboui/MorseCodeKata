962809dca770b96cfc71a643001d611cee4c4b41
samehsboui <samehsboui.enicar@gmail.com>
2019-11-06 17:26:20 +0100

    changing code using map : according codes to eatch letters into that map + splitting the input by '   ' then replacing the codes by letters and recollect and join results by ' '

---
+++ src/main/java/fr/runadium/MorseCode/DecodingMorse.java
@@ -0,0 +1,51 @@
+package fr.runadium.MorseCode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class DecodingMorse {
+
+
+	private static String[]  lettersAndNumbers = { "1", "2", "3", "4",
+			"5", "6", "7", "8", "9", "0" ,"A", "B", "C", "D", "E", "F", 
+			"G", "H", "I", "J", "k", "L", "M", "N", "O", "P", "Q", "R",
+			"S", "T", "U", "V", "W", "X", "Y", "Z"};
+	private static String[] equivalentCode = { ".----",
+			"..---", "...--", "....-", ".....", "-....",
+			"--...", "---..", "----.", "-----" ,".-",
+			"-...", "-.-.", "-..", ".", "..-.", "--.", "....", 
+			"..", ".---", "-.-", ".-..", "--",
+			"-.", "---", ".--.", "--.-", ".-.", "...", "-",
+			"..-", "...-", ".--", "-..-", "-.--", "--.."};
+	
+	private static Map<String, String> containerOfLettersAndCode = new HashMap<String, String>();
+
+	private DecodingMorse() {}
+	
+	public static void put() {
+		for (int i = 0; i < lettersAndNumbers.length; i++) {
+			containerOfLettersAndCode.put(equivalentCode[i], lettersAndNumbers[i]);
+		  }		
+	}
+	
+	public static String get(String value) {
+		String lettersAndNumbers = "";
+		if (containerOfLettersAndCode.containsKey(value)) {
+			lettersAndNumbers = containerOfLettersAndCode.get(value);
+		}
+		return lettersAndNumbers;
+	}
+	
+	
+	public static String decodeMorse(String inputMorseCode) {
+		return Arrays.stream(inputMorseCode.trim().split("   ")).map(e->DecodingMorse.findEquivalent(e)).collect(Collectors.joining(" "));
+	}
+	
+	public static String findEquivalent(String words) {
+		DecodingMorse.put();
+		return Arrays.stream(words.split(" ")).map(e->DecodingMorse.get(e)).collect(Collectors.joining());
+	}
+	
+}
