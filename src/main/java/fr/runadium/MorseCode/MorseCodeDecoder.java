7de1338aeff1cc021091ab7cc81f445e6d903b09
samehsboui <samehsboui.enicar@gmail.com>
2019-11-13 11:02:40 +0100

    adding part 2/3 of the kata

--- src/main/java/fr/runadium/MorseCode/MorseCodeDecoder.java
+++ src/main/java/fr/runadium/MorseCode/MorseCodeDecoder.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class DecodingMorse {
+public class MorseCodeDecoder {
 
 	private static String[] lettersAndNumbers = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "A", "B", "C", "D",
 			"E", "F", "G", "H", "I", "J", "k", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y",
@@ -16,7 +16,7 @@ public class DecodingMorse {
 
 	private static Map<String, String> containerOfLettersAndCode = new HashMap<String, String>();
 
-	private DecodingMorse() {
+	private MorseCodeDecoder() {
 	}
 
 	public static void put() {
@@ -36,13 +36,13 @@ public class DecodingMorse {
 	}
 
 	public static String decodeMorse(String inputMorseCode) {
-		return Arrays.stream(inputMorseCode.trim().split("   ")).map(e -> DecodingMorse.findEquivalent(e))
+		return Arrays.stream(inputMorseCode.trim().split("   ")).map(e -> MorseCodeDecoder.findEquivalent(e))
 				.collect(Collectors.joining(" "));
 	}
 
 	public static String findEquivalent(String words) {
-		DecodingMorse.put();
-		return Arrays.stream(words.split(" ")).map(e -> DecodingMorse.get(e)).collect(Collectors.joining());
+		MorseCodeDecoder.put();
+		return Arrays.stream(words.split(" ")).map(e -> MorseCodeDecoder.get(e)).collect(Collectors.joining());
 	}
 
 }
--- src/test/java/fr/runadium/MorseCode/LettersAndTheirEquivalentTest.java
+++ src/test/java/fr/runadium/MorseCode/LettersAndTheirEquivalentTest.java
@@ -2,7 +2,6 @@ package fr.runadium.MorseCode;
 
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.rules.ExpectedException;
@@ -15,45 +14,50 @@ public class LettersAndTheirEquivalentTest {
 	@Test
 	public void correctInputNumberTest() {
 		String input = ".....";
-		assertEquals("5", DecodingMorse.decodeMorse(input));
+		assertEquals("5", MorseCodeDecoder.decodeMorse(input));
 	}
-	
 
 	@Test
 	public void incorrectInputTwoNumbersTest() {
 		String input = "--... .....";
-		assertNotEquals("15", DecodingMorse.decodeMorse(input));
+		assertNotEquals("15", MorseCodeDecoder.decodeMorse(input));
 	}
-	
+
 	@Test
 	public void correctInputTwoNumbersTest() {
 		String input = "--... .....";
-		assertEquals("75", DecodingMorse.decodeMorse(input));
+		assertEquals("75", MorseCodeDecoder.decodeMorse(input));
 	}
-	
+
 	@Test
 	public void incorrectInputLertterTest() {
 		String input = ".-";
-		assertEquals("A", DecodingMorse.decodeMorse(input));
+		assertEquals("A", MorseCodeDecoder.decodeMorse(input));
+	}
+
+	@Test
+	public void correctCombinningLertterWithNumberTest() {
+		String input = ".- --... .....";
+		assertEquals("A75", MorseCodeDecoder.decodeMorse(input));
 	}
 
 	@Test
 	public void patternGivenInKataTest() {
 		String input = ".... . -.--   .--- ..- -.. .";
-		assertEquals("HEY JUDE", DecodingMorse.decodeMorse(input));
+		assertEquals("HEY JUDE", MorseCodeDecoder.decodeMorse(input));
 	}
-	
+
 	@Test
 	public void wrongPhraseTest() {
 		String input = ".---- . -.--   .--- ..- -.. .";
-		assertNotEquals("Wrong", DecodingMorse.decodeMorse(input));
+		assertNotEquals("Wrong", MorseCodeDecoder.decodeMorse(input));
 	}
 
 	@Test
 	public void negativeInputThrown() {
 		expectedException.expect(IllegalArgumentException.class);
 		expectedException.expectMessage("Wrong input detected!!");
-		DecodingMorse.decodeMorse("-5");
+		MorseCodeDecoder.decodeMorse("-5");
 
 	}
 
